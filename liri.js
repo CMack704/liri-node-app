4ddd09f9a7b0a06a4720ac14ef9f7b53816b2041
Cmack704 <craigmack704@gmail.com>
2019-07-27 19:40:47 -0400

    made function for bands call and function for spotify call

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
-//var keys = require('./keys.js');
-//var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var newSpotify = new Spotify(keys.spotify);
 var axios = require("axios");
 //var fs = require('fs');
 var bandsUrl = "https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp"
@@ -12,20 +13,48 @@ var action = process.argv[2];
 
 switch (action) {
     case 'concert-this':
-        axios.get(bandsUrl).then(
-            function(response) {
-                console.log('Name of Venue: ' + response.data[0].venue.name);
-                console.log('Venue Location: ' + response.data[0].venue.city + ', ' + response.data[0].venue.region + ' ' + response.data[0].venue.country)
-                console.log('Event Date: ' + response.data[1].datetime)
-            })
-            .catch(function(error) {
-                console.log('error');
-            })
+        bandsInTown(process.argv.slice(2).join(""));
         break;
     case 'spotify-this-song':
+        spotify(process.argv[3])
         break;
     case 'movie-this':
         break;
     case 'do-what-it-says':
         break;
+}
+
+function bandsInTown(artist) {
+    axios.get(bandsUrl).then(
+        function (response) {
+
+            console.log('Name of Venue: ' + response.data[0].venue.name);
+            console.log('Venue Location: ' + response.data[0].venue.city + ', ' + response.data[0].venue.region + ' ' + response.data[0].venue.country)
+            console.log('Event Date: ' + response.data[1].datetime)
+        })
+        .catch(function (error) {
+            console.log('error');
+        })
+}
+function spotify(track) {
+    var track;
+    if (process.argv[3] === undefined) {
+        track = 'The Sign ace of base'
+    } else {
+        track = process.argv[3]
+    }
+    newSpotify
+        .search({ type: 'track', query: track })
+        .then(function (response) {
+            console.log('Artist: ' + response.tracks.items[0].artists[0].name);
+            console.log('Song Title: ' + response.tracks.items[0].name)
+            console.log('Listen to the Song: ' + response.tracks.items[0].external_urls.spotify)
+            console.log('Album: ' + response.tracks.items[0].album.name)
+        })
+        .catch(function (error) {
+            console.log('error');
+        });
+}
+function movieThis(movieName) {
+    
 }
