30fff5c872cf4a1a4988e9ae9cdc83b026fd6074
Cmack704 <craigmack704@gmail.com>
2019-07-27 20:55:19 -0400

    made do this function to read random.txt file and spotify song and write to random.txt

--- liri.js
+++ liri.js
@@ -4,8 +4,7 @@ var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 var newSpotify = new Spotify(keys.spotify);
 var axios = require("axios");
-//var fs = require('fs');
-
+var fs = require('fs');
 var action = process.argv[2];
 
 
@@ -13,20 +12,21 @@ var action = process.argv[2];
 
 switch (action) {
     case 'concert-this':
-        bandsInTown(process.argv.slice(2).join(" "));
+        bandsInTown(process.argv[3]);
         break;
     case 'spotify-this-song':
-        spotify(process.argv.slice(2).join(" "))
+        spotify(process.argv[3])
         break;
     case 'movie-this':
-        movieThis(process.argv.slice(2).join(" "))
+        movieThis(process.argv[3])
         break;
     case 'do-what-it-says':
+        doThis(process.argv[3])
         break;
 }
 
 function bandsInTown(artist) {
-    var band = process.argv.slice(2).join(" ");
+    var band = process.argv[3];
     var bandsUrl = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp";
     axios.get(bandsUrl).then(
         function (response) {
@@ -62,7 +62,7 @@ function movieThis(movieName) {
     if (process.argv[3] === undefined) {
         movie = 'Mr. Nobody';
     } else {
-        movie = process.argv.slice(2).join(" ");
+        movie = process.argv[3];
     }
     var omdbUrl = "http://www.omdbapi.com/?t=" + movie + "&apikey=ea09baf7"
     axios.get(omdbUrl).then(
@@ -70,7 +70,7 @@ function movieThis(movieName) {
             console.log("Title: " + response.data.Title);
             console.log("Year Released: " + response.data.Released);
             console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Source + response.data.Ratings[1].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Source + " " + response.data.Ratings[1].Value);
             console.log("Countries Movie was Produced in: " + response.data.Country);
             console.log("Movie Languages: " + response.data.Language);
             console.log("Plot of the Movie: " + response.data.Plot);
@@ -79,4 +79,31 @@ function movieThis(movieName) {
         .catch(function (error) {
             console.log('error');
         })
+}
+function doThis(param) {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log('error');
+        }
+        var dataArr = data.split(",");
+        newSpotify
+        .search({ type: 'track', query: dataArr[1] })
+        .then(function (response) {
+            console.log('Artist: ' + response.tracks.items[0].artists[0].name);
+            console.log('Song Title: ' + response.tracks.items[0].name)
+            console.log('Listen to the Song: ' + response.tracks.items[0].external_urls.spotify)
+            console.log('Album: ' + response.tracks.items[0].album.name)
+        })
+        .catch(function (error) {
+            console.log('error');
+        });      
+      });
+    var text = ',movie-this,"Interstellar",concert-this,"future"'
+    fs.appendFile("random.txt", text, function(error){
+        if (error) {
+            console.log('error');
+        } else {
+            console.log('success');
+        }
+    })
 }
